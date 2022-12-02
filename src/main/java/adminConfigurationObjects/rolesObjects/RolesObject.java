0fca5a93aa28d7c4752c4699b835535916f7eb15
PradPatil <pradnya.patil2@capita.com>
2022-12-02 16:10:01 +0530

    Second Commit

---
+++ src/main/java/adminConfigurationObjects/rolesObjects/RolesObject.java
@@ -0,0 +1,5 @@
+package adminConfigurationObjects.rolesObjects;
+
+public class RolesObject {
+
+}
---
+++ src/test/java/adminConfigurationTests/roles/RolesPage.java
@@ -0,0 +1,12 @@
+package adminConfigurationTests.roles;
+
+public class RolesPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing Role");
+
+	}
+
+}
